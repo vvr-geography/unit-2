39793b696dc833362ead260cc90d308b18fc9636
viggyram-geog <vramachandr9@wisc.edu>
2022-03-02 14:56:42 -0600

    Activity 6 upload
    Updated js code with retrieve and sequence through the years

--- Chapter 5/js/main.js
+++ Chapter 5/js/main.js
@@ -1,6 +1,7 @@
 var map;
 var minValue;
 
+//creates the base map and runs the primary fetch functions
 function createMap() {
 
     map = L.map('map').setView([34, -111], 4);
@@ -13,46 +14,40 @@ function createMap() {
     getBorderWall();
 }
 
-function calculateMinValue(json){
-     //create empty array to store all data values
-     var allValues = [];
-     //loop through each sector 
-     for (var sector of json.features){
-         for (var year = 2014; year <= 2020; year+=1){
-             //get apprehension data for current year
-             var value = sector.properties["FY" + String(year) +"_total"];
-             //add value to array
-             allValues.push(value);
-         }
-     }
-     //get minimum value of our array
-     var minValue = Math.min(...allValues)
+function calculateMinValue(json) {
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each sector 
+    for (var sector of json.features) {
+        for (var year = 2014; year <= 2020; year += 1) {
+            //get apprehension data for current year
+            var value = sector.properties["FY" + String(year) + "_total"];
+            //add value to array
+            allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
 
-     return minValue;
+    return minValue;
 }
 
 function calcPropRadius(attValue) {
-     //constant factor adjusts symbol sizes evenly
-     var minRadius = .15;
-     //Flannery Apperance Compensation formula
-     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = .15;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue / minValue, 0.5715) * minRadius
 
-     return radius;
+    return radius;
 };
 
-function onEachFeature(feature, layer) {
-    var popupContent = "";
-    if (feature.properties) {
-        for (var property in feature.properties) {
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-    } layer.bindPopup(popupContent);
-};
+//function to convert markers into circle markers
+function pointToLayer(feature, latlng, attributes) {
 
-function createPropSymbols(json) {
-    
-    var attribute = "FY2014_total";
-    
+    //Determine which attribute to visualize with proportional symbols)
+    var attribute = attributes[0];
+
+    //create marker options
     var geojsonMarkerOptions = {
         radius: 8,
         fillColor: "#ff7800",
@@ -62,16 +57,132 @@ function createPropSymbols(json) {
         fillOpacity: 0.8
     };
 
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //give each feature's circle marker a radius based on its attribute value
+    geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, geojsonMarkerOptions);
+
+    //build popup content string
+    var popupContent = "<p><b>CBP Sector Name: </b>" + feature.properties.sector + "</p>";
+
+    //add year to pop
+    var year = attribute.split("_")[0];
+
+    popupContent += "<p><b>Border Apprehensions in " + year + ": </b>" + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJSON pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(json, attributes) {
+
+    //create a Leaflet geoJSON layer and add it to the map
     L.geoJSON(json, {
-        pointToLayer: function (feature, latlng) {
-            var attValue = Number(feature.properties[attribute]);
+        pointToLayer: function (feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map)
+};
 
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+function createSequenceControls(attributes) {
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend', slider);
 
-            return L.circleMarker(latlng, geojsonMarkerOptions)
-        },
-        onEachFeature: onEachFeature
-    }).addTo(map);
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    document.querySelector('#panel').insertAdjacentHTML('beforeend', '<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend', '<button class="step" id="forward"></button>');
+
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend', "<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend', "<img src='img/forward.png'>")
+
+    //add click listener for buttons
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            //increment or decrement depending on button clicked
+            if(step.id == 'forward'){
+                index++;
+                //if past the last attribute, wrap arround to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index --;
+                //if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
+            };
+
+            //update slider
+            document.querySelector('.range-slider').value = index;
+
+            //pass new attribute to update symbols
+            updatePropSymbols(attributes[index]);
+        })
+    })
+
+    //input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //get the new index value
+        var index = this.value;
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+//Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            var props = layer.feature.properties
+
+            //update the layer style and popup
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+           
+            //add city to popup content string
+            var popupContent = "<p><b>CBP Sector Name: </b>" + props.sector + "</p>";
+
+            //add year to pop
+            var year = attribute.split("_")[0];
+
+            popupContent += "<p><b>Border Apprehensions in " + year + ": </b>" + props[attribute] + "</p>";
+
+            //update popup content
+            popup = layer.getPopup();
+            popup.setContent(popupContent).update();
+        };
+    });
+};
+
+//build an attributes array from data
+function processData(json){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of first feature in the data set
+    var properties = json.features[0].properties;
+
+    //push each attribute name into the attribute array
+    for (var attribute in properties) {
+        //only take attributes with FY data
+        if (attribute.indexOf("FY") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+
 };
 
 
@@ -81,11 +192,15 @@ function getData() {
             return response.json();
         })
         .then(function (json) {
+            //create attributes array
+            var attributes = processData(json);
             minValue = calculateMinValue(json);
-            createPropSymbols(json);
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
         })
 };
 
+//fetches the borderwall data and styles it
 function getBorderWall() {
     fetch('data/border_fence_map.geojson')
         .then(function (response) {
@@ -108,3 +223,5 @@ function getBorderWall() {
 };
 
 document.addEventListener('DOMContentLoaded', createMap);
+
+//activity 6 commit
